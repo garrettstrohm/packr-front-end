b516453b24b8c9474d2a109c0896dd82afb20248
Garrett <garres4@vt.edu>
2021-12-06 20:07:15 -0700

    created landing page route

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import NavBar from './NavBar';
+import LandingPage from './landingPage';
 import { Route, Switch } from "react-router-dom";
 
 function App() {
@@ -7,7 +8,9 @@ function App() {
     <div className="App">
       <NavBar/>
       <Switch>
-        
+        <Route path="/">
+          <LandingPage />
+        </Route>
       </Switch>
     </div>
   );
